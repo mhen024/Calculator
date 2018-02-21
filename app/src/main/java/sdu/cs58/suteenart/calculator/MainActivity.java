3e00760918dcb57c6a04017b1d38999a2427e4c5
mhen <mhen008@gmail.com>
2018-02-21 11:43:18 +0700

    add complete boiiiiiiiii

--- app/src/main/java/sdu/cs58/suteenart/calculator/MainActivity.java
+++ app/src/main/java/sdu/cs58/suteenart/calculator/MainActivity.java
@@ -2,12 +2,39 @@ package sdu.cs58.suteenart.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText number1EditText,number2EditText;
+    Button addButton;
+    TextView resultTextView;
+    int num1, num2, result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        //2. ผูกตัวแปรบน java กับ element บน xml
+        number1EditText = findViewById(R.id.edtNumber1);
+        number2EditText = findViewById(R.id.edtNumber2);
+        addButton = findViewById(R.id.btnAdd);
+        resultTextView = findViewById(R.id.txvResult);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //3.แปลงค่าจาก Edittext เป็นตัวเลขในตัวแปร int
+                num1 = Integer.parseInt(number1EditText.getText().toString().trim());
+                num2 = Integer.parseInt(number2EditText.getText().toString().trim());
+                result = num1 + num2;
+                //4. แสดงผลลับการคำนวน
+                resultTextView.setText(result + "");
+            }
+        });//end setOnClickListener
+
+    } //end onCreate
+} //end Class
