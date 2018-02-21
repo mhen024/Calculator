a53c85116ebf6143b20668670ab4c548bbbcfa0d
mhen <mhen008@gmail.com>
2018-02-21 12:15:52 +0700

    4Button

--- app/src/main/java/sdu/cs58/suteenart/calculator/MainActivity.java
+++ app/src/main/java/sdu/cs58/suteenart/calculator/MainActivity.java
@@ -10,9 +10,9 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     EditText number1EditText,number2EditText;
-    Button addButton;
+    Button addButton,minusButton,mlutiButton,dividedButton;
     TextView resultTextView;
-    int num1, num2, result;
+    int num1, num2, result, result2, result3, result4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
         number1EditText = findViewById(R.id.edtNumber1);
         number2EditText = findViewById(R.id.edtNumber2);
         addButton = findViewById(R.id.btnAdd);
+        minusButton = findViewById(R.id.btnMinus);
+        mlutiButton = findViewById(R.id.btnMluti);
+        dividedButton = findViewById(R.id.btnDivided);
         resultTextView = findViewById(R.id.txvResult);
 
         addButton.setOnClickListener(new View.OnClickListener() {
@@ -33,8 +36,37 @@ public class MainActivity extends AppCompatActivity {
                 result = num1 + num2;
                 //4. แสดงผลลับการคำนวน
                 resultTextView.setText(result + "");
+
             }
         });//end setOnClickListener
+        minusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = Integer.parseInt(number1EditText.getText().toString().trim());
+                num2 = Integer.parseInt(number2EditText.getText().toString().trim());
+                result2 = num1 - num2;
+                resultTextView.setText(result2 + "");
+            }
+        });
+        mlutiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = Integer.parseInt(number1EditText.getText().toString().trim());
+                num2 = Integer.parseInt(number2EditText.getText().toString().trim());
+                result3 = num1 * num2;
+                resultTextView.setText(result3 + "");
+            }
+        });
+        dividedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = Integer.parseInt(number1EditText.getText().toString().trim());
+                num2 = Integer.parseInt(number2EditText.getText().toString().trim());
+                result4 = num1 / num2;
+                resultTextView.setText(result4 + "");
+            }
+        });
+
 
     } //end onCreate
 } //end Class
